68bb93f44b3786d5a7922709447b7bdde08c76d6
Jorgelina-Etchevest <etchevestjor@gmail.com>
2022-07-17 11:52:05 -0300

    Avance Proyecto Javascript Avanzado

---
+++ js/video.js
@@ -0,0 +1,12 @@
+let video= document.querySelector(".video"); 
+
+let duracion=document.querySelector(".duracion")
+
+function inicio() {
+    video.play()
+    duracion.textContent=`Duración Video: ${video.duration}`
+}
+function pausar() {
+    video.pause()
+}
+
