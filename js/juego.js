398d75c74983a94990ad0811099573f1104afe85
Jorgelina-Etchevest <etchevestjor@gmail.com>
2022-08-06 15:03:50 -0300

    otro cambio

--- js/juego.js
+++ js/juego.js
@@ -3,12 +3,10 @@
 let primera_imagen=document.querySelector(".imagen_tres");
 
 primera_imagen.addEventListener('dragstart',(evento)=>{
-    console.log("se inicio el arrastre de la imagen");
     evento.dataTransfer.setData("Text","./imagenes/rompe3.png");
 })
 
 primera_imagen.addEventListener('dragend',()=>{
-    console.log("se soltó la imagen");
     primera_imagen.style.display="none";
 })
 
@@ -17,12 +15,10 @@ primera_imagen.addEventListener('dragend',()=>{
 let segunda_imagen=document.querySelector(".imagen_dos");
 
 segunda_imagen.addEventListener('dragstart',(evento)=>{
-    console.log("se inicio el arrastre de la imagen");
     evento.dataTransfer.setData("Text","./imagenes/rompe2.png");
 })
 
 segunda_imagen.addEventListener('dragend',()=>{
-    console.log("se soltó la imagen");
     segunda_imagen.style.display="none";
 })
 
@@ -31,12 +27,10 @@ segunda_imagen.addEventListener('dragend',()=>{
 let tercera_imagen=document.querySelector(".imagen_uno");
 
 tercera_imagen.addEventListener('dragstart',()=>{
-    console.log("se inicio el arrastre de la imagen");
     evento.dataTransfer.setData("Text","./imagenes/rompe1.png");
 })
 
 tercera_imagen.addEventListener('dragend',()=>{
-    console.log("se soltó la imagen");
     tercera_imagen.style.display="none";
 })
 
@@ -47,13 +41,11 @@ tercera_imagen.addEventListener('dragend',()=>{
 let zona_uno = document.querySelector('.rompecabezas_uno')
 
 zona_uno.addEventListener('dragover',(evento)=>{
-    console.log("dentro de zona de destino");
     evento.preventDefault();
 })
 
 zona_uno.addEventListener('drop',(evento)=>{
     let info=evento.dataTransfer.getData("Text");
-    console.log(info)
     zona_uno.innerHTML=`<img src="${info}"/>`;  
 }) 
 
@@ -62,13 +54,11 @@ zona_uno.addEventListener('drop',(evento)=>{
 let zona_dos = document.querySelector('.rompecabezas_dos')
 
 zona_dos.addEventListener('dragover',(evento)=>{
-    console.log("dentro de zona de destino");
     evento.preventDefault();
 })
 
 zona_dos.addEventListener('drop',(evento)=>{
     let info=evento.dataTransfer.getData("Text");
-    console.log(info)
     zona_dos.innerHTML=`<img src="${info}"/>`;  
 })
 
@@ -76,13 +66,11 @@ zona_dos.addEventListener('drop',(evento)=>{
 let zona_tres = document.querySelector('.rompecabezas_tres')
 
 zona_tres.addEventListener('dragover',(evento)=>{
-    console.log("dentro de zona de destino");
     evento.preventDefault();
 })
 
 zona_tres.addEventListener('drop',(evento)=>{
     let info=evento.dataTransfer.getData("Text");
-    console.log(info)
     zona_tres.innerHTML=`<img src="${info}"/>`;  
 })
  
